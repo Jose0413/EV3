4589064176f194663f0282fcebdd7f7c56e7ca03
Jose0413 <jose.sepulveda0413@gmail.com>
2021-06-28 23:34:11 -0400

    h

--- rest_mascota/urls.py
+++ rest_mascota/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_mascota.views import lista_mascotas
+from rest_mascota.views import detalle_mascota
 
 urlpatterns = [
     path('lista_mascotas/', lista_mascotas, name="lista_mascotas"),
+    path('detalle_mascota/<str:codigo>/', detalle_mascota, name="detalle_mascota"),
 ]
--- rest_mascota/views.py
+++ rest_mascota/views.py
@@ -5,13 +5,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.serializers import Serializer
 from AppPeludo.models import Mascotas
 from .serializers import MascotasSerializer
+
 @csrf_exempt
 @api_view(['GET','POST'])
 def lista_mascotas(request):
     if request.method == 'GET':
-
         mascota = Mascotas.objects.all()
         serializer = MascotasSerializer(mascota, many=True)
         return Response(serializer.data)
@@ -25,4 +26,26 @@ def lista_mascotas(request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET','PUT','DELETE'])
+def detalle_mascota(request, codigo):
+    try:
+        mascota = Mascotas.objects.get(codigo=codigo)
+    except Mascotas.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET':
+        serializer = MascotasSerializer(mascota)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = MascotasSerializer(mascota,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        mascota.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
